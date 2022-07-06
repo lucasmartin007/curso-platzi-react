fd6e0fd73b21a52d3898eb0ae34f2ab344050999
lucasmartin007 <gmoreno@ithreexglobal.com>
2022-07-06 15:32:28 -0300

    curso react

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+// console.log("Empezamos. Curso de React");
+
+//ReactDOM.render("Curso de React", document.body);
+
+ReactDOM.render("Curso de React avanzado", document.getElementById("div_app"));
